a628cb0c0e66b30cb84d665c7639618c2663de64
Justin <Justin>
2020-11-07 07:10:13 +0800

    changes-11-17-2020

---
+++ src/pages/gallery.js
@@ -0,0 +1,17 @@
+import React from "react"
+import Layout from "../components/layout"
+import HelenaAuthor from "../assets/img/author_helena.jpg"
+import KarenAuthor from "../assets/img/author_karen.jpg"
+import Video from "../assets/gallery/video.mov"
+const Gallery = () => (
+  <Layout>
+  	<section className="page-wrapper">
+  		<div id="video-wrapper">
+  		
+  		</div>
+	</section>
+  </Layout>
+)
+
+export default Gallery
+	

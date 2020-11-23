8d03b952509f8f7214e960ce85b839c5cd768fc3
Justin <Justin>
2020-11-23 22:33:49 +0800

    changes-11-23-2020

--- src/pages/gallery.js
+++ src/pages/gallery.js
@@ -2,7 +2,7 @@ import React from "react"
 import Layout from "../components/layout"
 import HelenaAuthor from "../assets/img/author_helena.jpg"
 import KarenAuthor from "../assets/img/author_karen.jpg"
-import Video from "../assets/gallery/video.mov"
+
 const Gallery = () => (
   <Layout>
   	<section className="page-wrapper">
